2502869fa430763b6d60b8f01627b015df58e8ab
Mamytov <96724591+Abdillamit@users.noreply.github.com>
2024-09-23 14:32:42 +0600

    Fixed/pipeline

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,3 @@
-import wrapWithProvider from './src/redux/wrap-with-provider';
+import wrapWithProvider from './src/redux/wrap-with-provider-ssr';
 
 export const wrapRootElement = wrapWithProvider;
---
+++ src/components/devices/getDevices/redux/ssr-reducer.ts
@@ -0,0 +1,23 @@
+/* Local dependencies */
+
+import { GetDevicesRequest } from "./action";
+
+export interface GetDevicesState {
+  loading: boolean;
+  error: null | Error;
+}
+
+export const initialGetDevicesState: GetDevicesState = {
+  loading: false,
+  error: null,
+};
+
+export default function loginReducer(state = initialGetDevicesState, action: GetDevicesRequest) {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+}
+
+
